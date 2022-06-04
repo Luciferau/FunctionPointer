b7cd40ee9b4fbf71d698a7191bd530bf3fd22523
XY520 <3372819612@qq.com>
2022-06-04 16:36:37 +0800

    Add files via upload

---
+++ mian.cpp
@@ -0,0 +1,46 @@
+
+/*
+#include <stdio.h>
+
+void biaoBai() {
+	printf("I love you\n");
+}
+
+void biaoBai2() {
+	printf("x7 \n");
+}
+
+void biaoBai4() {
+	printf("A8");
+}
+
+void biaoBai3()
+{
+	printf("1");
+}
+
+void love(const char* girl, void (*p)()) {
+	printf("%s", girl);
+	 p();
+}
+
+int main(void)
+{
+	//定义一个函数指针，变量是p
+	//用来指向一种特殊的函数：void函数名（）
+
+	 
+	void(*p)();
+	p = &biaoBai;//指向p
+	p();//通过函数p来调用函数 完全等效于biaoBai（）
+	 
+
+	love("yf ", biaoBai);
+
+	return 0;
+
+
+}
+
+ */
+
